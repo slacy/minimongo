9144ef8da2ae21b3511b3c67d6b9d288d75a83d4
Steve Lacy <slacy@slacy.com>
2011-01-11 15:04:37 -0800

    Tweak to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
+README = open(os.path.join(here, 'README.rst')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = ['pymongo']
