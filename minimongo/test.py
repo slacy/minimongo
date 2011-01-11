7dc183f6df2debe254506f1b91be22099f85d601
Steve Lacy <slacy@slacy.com>
2011-01-10 19:13:52 -0800

    Some PEP8 cleanup

--- minimongo/test.py
+++ minimongo/test.py
@@ -3,10 +3,13 @@ import unittest
 from minimongo.model import Model, MongoCollection
 
 class TestModel(Model):
+    """Model class for test cases."""
     mongo = MongoCollection(database='test', collection='minimongo_test')
 
 
 def assertContains(iterator, instance):
+    """Given an iterable of Models, make sure that the instance (of a Model)
+    is inside the iterable."""
     for i in iterator:
         if i.rawdata == instance.rawdata:
             return True
@@ -14,109 +17,113 @@ def assertContains(iterator, instance):
 
 
 class TestSimpleModel(unittest.TestCase):
+    """Main test case."""
     def setUp(self):
+        """unittest setup, drop the whole collection before starting each test."""
         TestModel.drop()
 
     def test_creation(self):
-        m = TestModel({'x':1, 'y':1})
-        m.save()
-        n = TestModel.find_one({'x': 1})
+        """Test simple object creation and querying via find_one."""
+        dummy_m = TestModel({'x': 1, 'y': 1})
+        dummy_m.save()
+        dummy_n = TestModel.find_one({'x': 1})
         # Make sure that the find_one method returns the right type.
-        self.assertEqual(type(n), TestModel)
+        self.assertEqual(type(dummy_n), TestModel)
         # Make sure that the contents are the same.
-        self.assertEqual(n.rawdata, m.rawdata)
+        self.assertEqual(dummy_n.rawdata, dummy_m.rawdata)
 
     def test_queries(self):
-        a = TestModel({'x': 1, 'y': 1}).save()
-        b = TestModel({'x': 1, 'y': 2}).save()
-        c = TestModel({'x': 2, 'y': 2}).save()
-        d = TestModel({'x': 2, 'y': 1}).save()
+        """Test some more complex query forms."""
+        dummy_a = TestModel({'x': 1, 'y': 1}).save()
+        dummy_b = TestModel({'x': 1, 'y': 2}).save()
+        dummy_c = TestModel({'x': 2, 'y': 2}).save()
+        dummy_d = TestModel({'x': 2, 'y': 1}).save()
 
-        x1 = TestModel.find({'x': 1})
-        y1 = TestModel.find({'y': 1})
-        x2y2 = TestModel.find({'x': 2, 'y': 2})
+        found_x1 = TestModel.find({'x': 1})
+        found_y1 = TestModel.find({'y': 1})
+        found_x2y2 = TestModel.find({'x': 2, 'y': 2})
 
-        lx1 = list(x1)
-        ly1 = list(y1)
-        lx2y2 = list(x2y2)
+        list_x1 = list(found_x1)
+        list_y1 = list(found_y1)
+        list_x2y2 = list(found_x2y2)
 
-        self.assertEqual(type(lx1[0]), TestModel)
+        self.assertEqual(type(list_x1[0]), TestModel)
 
-        self.assertTrue(assertContains(lx1, a))
-        self.assertTrue(assertContains(lx1, b))
-        self.assertTrue(assertContains(ly1, a))
-        self.assertTrue(assertContains(ly1, d))
-        self.assertEqual(lx2y2[0].rawdata, c.rawdata)
+        self.assertTrue(assertContains(list_x1, dummy_a))
+        self.assertTrue(assertContains(list_x1, dummy_b))
+        self.assertTrue(assertContains(list_y1, dummy_a))
+        self.assertTrue(assertContains(list_y1, dummy_d))
+        self.assertEqual(list_x2y2[0].rawdata, dummy_c.rawdata)
 
     def test_deletion(self):
-        m = TestModel()
-        m.x = 100
-        m.y = 200
-        m.save()
+        """Test deleting an object from a collection."""
+        dummy_m = TestModel()
+        dummy_m.x = 100
+        dummy_m.y = 200
+        dummy_m.save()
 
-        n = TestModel.find({'x': 100})
-        self.assertEqual(n.count(), 1)
-        for i in n:
+        dummy_n = TestModel.find({'x': 100})
+        self.assertEqual(dummy_n.count(), 1)
+        for i in dummy_n:
             i.delete()
 
-        m = TestModel.find({'x': 100})
-        self.assertEqual(m.count(), 0)
+        dummy_m = TestModel.find({'x': 100})
+        self.assertEqual(dummy_m.count(), 0)
 
     def test_complex_types(self):
         """Test lists as types."""
-        m = TestModel()
-        m.l = ['a', 'b', 'c']
-        m.x = 1
-        m.save()
+        dummy_m = TestModel()
+        dummy_m.l = ['a', 'b', 'c']
+        dummy_m.x = 1
+        dummy_m.save()
 
-        n = TestModel.find_one({'x': 1})
+        dummy_n = TestModel.find_one({'x': 1})
 
-        ml = m.l
-        nl = n.l
+        ml = dummy_m.l
+        nl = dummy_n.l
         self.assertEqual(ml, nl)
 
     def test_delete_field(self):
         """Test deleting a single field from an object."""
-        m = TestModel({'x': 1, 'y': 2})
-        m.save()
-        del(m.x)
-        m.save()
+        dummy_m = TestModel({'x': 1, 'y': 2})
+        dummy_m.save()
+        del(dummy_m.x)
+        dummy_m.save()
 
         n = TestModel.find_one({'y': 2})
-        self.assertEqual(n.rawdata, {'y': 2, '_id': m._id})
+        self.assertEqual(n.rawdata, {'y': 2, '_id': dummy_m._id})
 
     def test_count_and_fetch(self):
         """Test counting methods on Cursors. """
-        a = TestModel({'x': 1, 'y': 1}).save()
-        b = TestModel({'x': 1, 'y': 2}).save()
-        c = TestModel({'x': 1, 'y': 3}).save()
-        d = TestModel({'x': 1, 'y': 4}).save()
+        dummy_a = TestModel({'x': 1, 'y': 1}).save()
+        dummy_b = TestModel({'x': 1, 'y': 2}).save()
+        dummy_c = TestModel({'x': 1, 'y': 3}).save()
+        dummy_d = TestModel({'x': 1, 'y': 4}).save()
 
-        m = TestModel.find({'x': 1}).sort('y')
-        self.assertEqual(m.count(), 4)
-        l = list(m)
-        self.assertEqual(l[0].rawdata, a.rawdata)
-        self.assertEqual(l[1].rawdata, b.rawdata)
-        self.assertEqual(l[2].rawdata, c.rawdata)
-        self.assertEqual(l[3].rawdata, d.rawdata)
-
+        find_x1 = TestModel.find({'x': 1}).sort('y')
+        self.assertEqual(find_x1.count(), 4)
+        list_x1 = list(find_x1)
+        self.assertEqual(list_x1[0].rawdata, dummy_a.rawdata)
+        self.assertEqual(list_x1[1].rawdata, dummy_b.rawdata)
+        self.assertEqual(list_x1[2].rawdata, dummy_c.rawdata)
+        self.assertEqual(list_x1[3].rawdata, dummy_d.rawdata)
 
     def test_dbref(self):
         """Test generation of DBRef objects, and querying via DBRef
         objects."""
-        a = TestModel({'x': 1, 'y': 999}).save()
-        a1 = a.dbref()
+        dummy_a = TestModel({'x': 1, 'y': 999}).save()
+        ref_a = dummy_a.dbref()
 
-        b = TestModel.from_dbref(a1)
-        self.assertEqual(a.rawdata, b.rawdata)
+        dummy_b = TestModel.from_dbref(ref_a)
+        self.assertEqual(dummy_a.rawdata, dummy_b.rawdata)
 
     def test_db_and_collection_names(self):
         """Test the methods that return the current class's DB and
         Collection names."""
-        a = TestModel({'x': 1})
-        self.assertEqual(a.database_name, 'test')
+        dummy_a = TestModel({'x': 1})
+        self.assertEqual(dummy_a.database_name, 'test')
         self.assertEqual(TestModel.database_name(), 'test')
-        self.assertEqual(a.collection_name, 'minimongo_test')
+        self.assertEqual(dummy_a.collection_name, 'minimongo_test')
         self.assertEqual(TestModel.collection_name(), 'minimongo_test')
 
 if __name__ == '__main__':
